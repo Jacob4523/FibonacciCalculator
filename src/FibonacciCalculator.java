39352cd2ab98fbc157aee25ff9919b7cc2ece047
Jacob4523 <jcgaffke@oakland.edu>
2024-11-16 16:15:58 -0500

    second commit

--- src/FibonacciCalculator.java
+++ src/FibonacciCalculator.java
@@ -1,14 +1,20 @@
-import java.util.Scanner;
-
 public class FibonacciCalculator {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter an integer: ");
-        int f = scanner.nextInt();
-        scanner.close();
-        
-        int result = fibonacci(f);
-        System.out.println("Fibonacci number at position " + f + " is " + result);
+        if (args.length != 1) {
+            System.out.println("Usage: java -jar FibonacciCalculator.jar <integer>");
+            return;
+        }
+
+        int fValue;
+        try {
+            fValue = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("The argument must be an integer.");
+            return;
+        }
+
+        int result = fibonacci(fValue);
+        System.out.println("Fibonacci number at position " + fValue + " is " + result);
     }
 
     public static int fibonacci(int n) {
